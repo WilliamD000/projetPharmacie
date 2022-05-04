df1befcd0a55097214110fc3fa06aa3dcc3d5b78
William Desjardins <deswi0001@gmail.com>
2022-05-04 08:31:58 -0400

    Ajoutez des fichiers projet.

---
+++ projetPharmacie/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace projetPharmacie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCreerPharmacie = new System.Windows.Forms.Button();
+            this.edtNomPharmacie = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtAdressePharmacie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edtVillePharmacie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edtCodePostalPharmacie = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edtProvincePharmacie = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.edtAnneeDImplantation = new System.Windows.Forms.TextBox();
+            this.label034 = new System.Windows.Forms.Label();
+            this.edtNoPharmacie = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.edtCourrielPharmacie = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.edtTelephonePharmacie = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Controls.Add(this.edtAnneeDImplantation);
+            this.panel1.Controls.Add(this.label034);
+            this.panel1.Controls.Add(this.edtNoPharmacie);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.edtCourrielPharmacie);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.edtTelephonePharmacie);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.edtProvincePharmacie);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.edtCodePostalPharmacie);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.edtVillePharmacie);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnCreerPharmacie);
+            this.panel1.Controls.Add(this.edtAdressePharmacie);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.edtNomPharmacie);
+            this.panel1.Location = new System.Drawing.Point(23, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 332);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCreerPharmacie
+            // 
+            this.btnCreerPharmacie.Location = new System.Drawing.Point(15, 288);
+            this.btnCreerPharmacie.Name = "btnCreerPharmacie";
+            this.btnCreerPharmacie.Size = new System.Drawing.Size(75, 28);
+            this.btnCreerPharmacie.TabIndex = 1;
+            this.btnCreerPharmacie.Text = "Créer";
+            this.btnCreerPharmacie.UseVisualStyleBackColor = true;
+            // 
+            // edtNomPharmacie
+            // 
+            this.edtNomPharmacie.Location = new System.Drawing.Point(15, 52);
+            this.edtNomPharmacie.Name = "edtNomPharmacie";
+            this.edtNomPharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtNomPharmacie.TabIndex = 2;
+            this.edtNomPharmacie.Text = "Familiprix";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Adresse";
+            // 
+            // edtAdressePharmacie
+            // 
+            this.edtAdressePharmacie.Location = new System.Drawing.Point(15, 98);
+            this.edtAdressePharmacie.Name = "edtAdressePharmacie";
+            this.edtAdressePharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtAdressePharmacie.TabIndex = 4;
+            this.edtAdressePharmacie.Text = "200 rue Témiscouata";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ville";
+            // 
+            // edtVillePharmacie
+            // 
+            this.edtVillePharmacie.Location = new System.Drawing.Point(15, 151);
+            this.edtVillePharmacie.Name = "edtVillePharmacie";
+            this.edtVillePharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtVillePharmacie.TabIndex = 6;
+            this.edtVillePharmacie.Text = "Rivière-du-Loup";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Code Postal";
+            // 
+            // edtCodePostalPharmacie
+            // 
+            this.edtCodePostalPharmacie.Location = new System.Drawing.Point(15, 201);
+            this.edtCodePostalPharmacie.Name = "edtCodePostalPharmacie";
+            this.edtCodePostalPharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtCodePostalPharmacie.TabIndex = 8;
+            this.edtCodePostalPharmacie.Text = "G5R2Y5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Province";
+            // 
+            // edtProvincePharmacie
+            // 
+            this.edtProvincePharmacie.Location = new System.Drawing.Point(15, 249);
+            this.edtProvincePharmacie.Name = "edtProvincePharmacie";
+            this.edtProvincePharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtProvincePharmacie.TabIndex = 10;
+            this.edtProvincePharmacie.Text = "Québec";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Pharmacie";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(143, 182);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(108, 13);
+            this.label.TabIndex = 19;
+            this.label.Text = "Annee D\'Implantation";
+            // 
+            // edtAnneeDImplantation
+            // 
+            this.edtAnneeDImplantation.Location = new System.Drawing.Point(143, 201);
+            this.edtAnneeDImplantation.Name = "edtAnneeDImplantation";
+            this.edtAnneeDImplantation.Size = new System.Drawing.Size(100, 20);
+            this.edtAnneeDImplantation.TabIndex = 18;
+            this.edtAnneeDImplantation.Text = "1977";
+            // 
+            // label034
+            // 
+            this.label034.AutoSize = true;
+            this.label034.Location = new System.Drawing.Point(143, 132);
+            this.label034.Name = "label034";
+            this.label034.Size = new System.Drawing.Size(74, 13);
+            this.label034.TabIndex = 17;
+            this.label034.Text = "No Pharmacie";
+            // 
+            // edtNoPharmacie
+            // 
+            this.edtNoPharmacie.Location = new System.Drawing.Point(143, 151);
+            this.edtNoPharmacie.Name = "edtNoPharmacie";
+            this.edtNoPharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtNoPharmacie.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(143, 79);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Courriel";
+            // 
+            // edtCourrielPharmacie
+            // 
+            this.edtCourrielPharmacie.Location = new System.Drawing.Point(143, 98);
+            this.edtCourrielPharmacie.Name = "edtCourrielPharmacie";
+            this.edtCourrielPharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtCourrielPharmacie.TabIndex = 14;
+            this.edtCourrielPharmacie.Text = "experienceclient@familiprix.com";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(143, 33);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 13);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Telephone";
+            // 
+            // edtTelephonePharmacie
+            // 
+            this.edtTelephonePharmacie.Location = new System.Drawing.Point(143, 52);
+            this.edtTelephonePharmacie.Name = "edtTelephonePharmacie";
+            this.edtTelephonePharmacie.Size = new System.Drawing.Size(100, 20);
+            this.edtTelephonePharmacie.TabIndex = 12;
+            this.edtTelephonePharmacie.Text = "418-862-2176";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Programme pharmacie";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox edtAnneeDImplantation;
+        private System.Windows.Forms.Label label034;
+        private System.Windows.Forms.TextBox edtNoPharmacie;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox edtCourrielPharmacie;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox edtTelephonePharmacie;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edtProvincePharmacie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox edtCodePostalPharmacie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox edtVillePharmacie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreerPharmacie;
+        private System.Windows.Forms.TextBox edtAdressePharmacie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edtNomPharmacie;
+    }
+}
+
